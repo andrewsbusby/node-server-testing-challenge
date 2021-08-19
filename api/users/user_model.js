8abf50508160e0a2eb39b62d1cd796cc48841ff1
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-08-19 17:43:04 -0600

    user_model set up

--- api/users/user_model.js
+++ api/users/user_model.js
@@ -4,6 +4,24 @@ const getAll = () => {
     return db('users')
 }
 
+const getById = (id) => {
+    return db('users').where('user_id', id).first()
+}
+
+const add = async (user) => {
+    const [id] = await db('users').insert(user)
+    return db('users').where('user_id', id).first()
+}
+
+const remove = async (id) => {
+    const bye = await db('users').where('user_id', id).first()
+    await db('users').where('user_id', id).del()
+    return bye
+}
+
 module.exports = {
     getAll,
+    getById,
+    add,
+    remove
 }
